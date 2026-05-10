5e17162c3d61187ca92ab0719c323569c89e2dba
Aditya <adi8yaaa@gmail.com>
2026-05-11 02:22:29 +0530

    connectivity check

--- src/utils/connectivityCheck.ts
+++ src/utils/connectivityCheck.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-
-const prisma = new PrismaClient();
 
 export async function checkConnectivity() {
   try {
